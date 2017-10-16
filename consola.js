1d509061f2b726357268158ab3be2b43a43e7c15
alferosillo <tallerdeaprendizaje.55@gmail.com>
2017-10-16 14:47:22 +0200

    subido todo

--- consola.js
+++ consola.js
@@ -1,28 +1,29 @@
 
 //
-//var pares=0;
-//var nones=1;
-//
-//var jugador =nones,maquina;
-//if(jugador==="pares"){
-//jugador=0;
-//maquina=1;
-//}
-//if(jugador==="nones"){
-//jugador=1;
-//maquina=0;
-//}
-//var jugadaJugador =7;
-//console.log(jugadaJugador);
-//var jugadaMaquina= Math.floor(Math.random()*11);
-//console.log(jugadaMaquina);
-//var resultados=(jugadaJugador+jugadaMaquina)%2;
-//console.log ("Esto es el" + resultado);
-//if(resultado === jugador){
-//console.log("gana jugador");
-//}
-//var resultados=(jugadaJugador+jugadaMaquina)%2;
-//console.log("Esto es el" + resultado);
-//if(resultado === maquina){
-//console.log("gana maquina");
-//}
+var pares=0;
+var nones=1;
+
+var jugador =nones,maquina;
+if(jugador==="pares"){
+jugador=0;
+maquina=1;
+}
+if(jugador==="nones"){
+jugador=1;
+maquina=0;
+}
+var jugadaJugador =7;
+console.log(jugadaJugador);
+var jugadaMaquina= Math.floor(Math.random()*11);
+console.log(jugadaMaquina);
+var resultados=(jugadaJugador+jugadaMaquina)%2;
+console.log ("Esto es el" + resultado);
+if(resultado === jugador){
+console.log("gana jugador");
+}
+var resultados=(jugadaJugador+jugadaMaquina)%2;
+console.log("Esto es el" + resultado);
+if(resultado === maquina){
+console.log("gana maquina");
+}
+
--- script.js
+++ script.js
@@ -9,23 +9,68 @@ document.getElementById("bienvenida").style.display='none';
 document.getElementById("elige").style.display='block';
 }
 
-document.getElementById("pares").onclick =function(){
+document.getElementById("siguiente").onclick =function(){
 document.getElementById("elige").style.display='none';
 document.getElementById("jugador").style.display='block';
 }
-document.getElementById("nones").onclick =function(){
-document.getElementById("elige").style.display='none';
-document.getElementById("jugador").style.display='block';
-}
+
 document.getElementById("continua").onclick =function(){
 document.getElementById("jugador").style.display='none';
 document.getElementById("maquina").style.display='block';
+  juego();  
 }
-document.getElementById("Juega").onclick =function(){
+document.getElementById("juega").onclick =function pantalla(){
 document.getElementById("maquina").style.display='none';
-document.getElementById("ganas").style.display='block'; 
+document.getElementById("ganas").style.display='block';
+   
 }
-/*document.getElementById("Juega").onclick =function(){
+document.getElementById("juega").onclick =function pantalla(){
 document.getElementById("maquina").style.display='none';
-document.getElementById("pierdes").style.display='block'; 
-}*/
+document.getElementById("pierdes").style.display='block';
+   
+}
+
+function juego (){
+var pares = document.getElementById("pares");
+ var nones = document.getElementById("nones");   
+   if(pares.checked){
+   var jugador= document.getElementById("pares").value;
+ var maquina =document.getElementById("nones").value;
+    
+   } 
+     if(nones.checked){
+   var jugador= document.getElementById("nones").value;
+ var maquina =document.getElementById("pares").value;
+    
+   }  
+console.log (jugador);
+console.log(maquina);
+   var jugada = document.getElementById("numero").value;
+    var jugadaMaquina =Math.floor(Math.random()*11);
+    console.log(jugada);
+    console.log(jugadaMaquina);
+    var suma =Number(jugada)+(jugadaMaquina);
+    console.log(suma);
+    
+if(suma%2===0){
+var resultado ="pares";
+}
+    if(suma%2 !==0){
+    var resultado ="nones";
+    
+    }
+    console.log(resultado);
+    
+if(resultado===jugador){
+document.getElementById("resultados").style.display= "block";
+    document.getElementById("pierdes").style.display= "none";
+/*var texto =jugada+jugadaMaquina;
+    document.getElementById("pantalla").innerHTML=texto;*/
+    
+}else {
+document.getElementById("pierdes").style.display= "block";
+    document.getElementById("resultados").style.display= "none";
+}
+}
+
+
